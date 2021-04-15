ed7bc2e00ccc29cb899c85136141b15abc87b596
Baz <bazfr@netc.fr>
2021-04-15 19:51:13 +0200

    Map base + mouvement + collision TODO (for the map part) : infinite scrolling

--- JamGame_Uncharted/Assets/Scripts/MapEngine.cs
+++ JamGame_Uncharted/Assets/Scripts/MapEngine.cs
@@ -4,11 +4,12 @@ using UnityEngine;
 
 public class MapEngine : MonoBehaviour
 {
-    ushort[,] tiles; // chunck ; tile
-    public int mapW, mapH, chunckW, chunckH;
+    ushort[][] tiles; // chunck ; tile
+    public int mapW, mapH, chunckS;
 
     ArrayList tilesGameobjects = new ArrayList();
     public Sprite[] tileSprites;
+    byte[] chunckID;
 
     public float chunckPerUnit = 1;
     private float hw;
@@ -18,24 +19,45 @@ public class MapEngine : MonoBehaviour
 
     void Start()
     {
+        chunckID = new byte[mapH * mapW];
+        chunckID[0] = 0b_0101_0000; //D R U L
+        chunckID[1] = 0b_0100_0001;
+        chunckID[2] = 0b_0101_0000;
+        chunckID[3] = 0b_0100_0001;
+        chunckID[4] = 0b_0100_0000;
+
+        chunckID[5] = 0b_0000_0100;
+        chunckID[6] = 0b_0001_0100;
+        chunckID[7] = 0b_0000_0101;
+        chunckID[8] = 0b_0001_0100;
+        chunckID[9] = 0b_0000_0101;
         hh = Camera.main.orthographicSize;
         hw = hh * Camera.main.aspect;
-        tiles = new ushort[mapH * mapW, chunckW * chunckH];
+        tiles = new ushort[mapH * mapW][];
         //bullshit filling
-        for (int c = 0; c < tiles.GetLength(0); c++)
+        for (int c = 0; c < tiles.Length; c++)
         {
-            for (int t = 0; t < tiles.GetLength(1); t++)
+            tiles[c] = chunkId2map(chunckID[c]);
+            /*tiles[c] = new ushort[chunckS * chunckS];
+            for (int t = 0; t < tiles[c].Length; t++)
             {
-                tiles[c, t] = (ushort)(c%2);
-            }
+                tiles[c][t] = (ushort)(c % 2);
+            }*/
         }
         //load the map
-        for (int c = 0; c < tiles.GetLength(0); c++)
+        for (int c = 0; c < tiles.Length; c++)
         {
-            Transform chunckTf = newGO(1/chunckPerUnit * ( c % mapW), -1/chunckPerUnit * ( c / mapH ), 0, 1/(float)chunckW, null, transform).GetComponent<Transform>();
-            for (int t = 0; t < tiles.GetLength(1); t++)
+            Transform chunckTf = newGO(1 / chunckPerUnit * (c % mapW), -1 / chunckPerUnit * (c / mapH), 0, 1 / (float)chunckS, null, transform).GetComponent<Transform>();
+            for (int t = 0; t < tiles[c].Length; t++)
             {
-                newGO(t % chunckW, -t / chunckH, 0, 1, tileSprites[tiles[c, t]], chunckTf);
+                GameObject go = newGO(t % chunckS, -t / chunckS, 0, 1, tileSprites[tiles[c][t]], chunckTf);
+                if (tiles[c][t] == 1)
+                {
+                    ((Rigidbody2D)go.AddComponent(typeof(Rigidbody2D))).bodyType = RigidbodyType2D.Static;
+                    BoxCollider2D box = (BoxCollider2D)go.AddComponent(typeof(BoxCollider2D));
+                    box.offset = new Vector2(-.003f, .003f);
+                    box.size = new Vector2(.125f, .125f);
+                }
             }
         }
     }
@@ -48,9 +70,9 @@ public class MapEngine : MonoBehaviour
         Center.position = Vector3.zero;
     }
 
-    public GameObject newGO(float x, float y, float rotation, float scale, Sprite sprite, Transform parent)
+    public GameObject newGO(float x, float y, float rotation, float scale, Sprite sprite, Transform parent) //0 empty, 1 is not
     {
-        scale *= (sprite == null) ? 1 : (sprite.texture.width/4); //assuming w = h
+        scale *= (sprite == null) ? 1 : (sprite.texture.width / 4); //assuming w = h
         GameObject g1 = new GameObject("(" + (x.ToString() + " ; " + y.ToString() + ")"), typeof(SpriteRenderer));
         SpriteRenderer sprRenderer = g1.GetComponent<SpriteRenderer>();
         sprRenderer.drawMode = SpriteDrawMode.Tiled;
@@ -64,4 +86,27 @@ public class MapEngine : MonoBehaviour
         return g1;
     }
 
+    ushort[] chunkId2map(byte chunck)
+    {
+        ushort[] _map = new ushort[chunckS * chunckS];
+        for (int d = 0; d<4; d++) //trough directions : R, U, L, D
+        {
+            bool open  = (chunck & (1 << d*2)) != 0;
+            int wallSize = (chunck & (2 << d*2)) == 0 ? 1 : (1+chunckS/4);
+            for (int i = 0; i < chunckS; i++)
+            {
+                for (int j = 0; j < wallSize; j++)
+                {
+                    switch (d)
+                    {
+                        case 0: _map[i * chunckS + j] = (ushort)((open && i == (chunckS / 2)) ? 0 : 1); break; //R
+                        case 1: _map[i + chunckS * j] = (ushort)((open && i == (chunckS / 2)) ? 0 : 1); break; //U
+                        case 2: _map[i * chunckS + (chunckS - 1 - j)] = (ushort)((open && i == (chunckS / 2)) ? 0 : 1); break; //L
+                        case 3: _map[i + chunckS * (chunckS - 1 - j)] = (ushort)((open && i == (chunckS / 2)) ? 0 : 1); break; //D
+                    }
+                }
+            }
+        }
+        return _map;
+    }
 }
---
+++ JamGame_Uncharted/Assets/Scripts/Movement.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class Movement : MonoBehaviour
+{
+    public float speed = 5f;
+    public Transform cameraTf;
+
+    void Update() {
+        Vector3 pos = cameraTf.position;
+        Vector2 npos = new Vector2(pos.x, pos.y);
+        if (Input.GetKey(KeyCode.DownArrow )) npos.y -= speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.UpArrow   )) npos.y += speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.RightArrow)) npos.x += speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow )) npos.x -= speed * Time.deltaTime;
+        GetComponent<Rigidbody2D>().MovePosition(npos);
+        cameraTf.position = new Vector3(transform.position.x, transform.position.y, cameraTf.position.z);
+        //transform.position = new Vector3(pos.x, pos.y, 1);
+    }
+}
